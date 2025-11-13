e349816b2b417fb53aab52ec9ab1c756fdfefb31
Nicholas Domanico <3966361+NicholasDomanico@users.noreply.github.com>
2025-11-13 14:36:44 -0500

    Add neovim dotfiles and plugins

---
+++ neovim/lua/options.lua
@@ -0,0 +1,18 @@
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.mouse = 'a'
+
+
+-- Tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+-- UI
+vim.opt.number = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.showmatch = true
