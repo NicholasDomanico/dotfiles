77ed3b197ce299774b0ed3ab7493c8b797e02e72
Nicholas Domanico <3966361+NicholasDomanico@users.noreply.github.com>
2025-12-06 09:43:44 -0500

    Tweak Picom settings, add checks for filetype to dwm_random_wallpaper.sh

--- scripts/dwm_random_walpaper.sh
+++ scripts/dwm_random_walpaper.sh
@@ -1,31 +1,55 @@
 #!/bin/sh
 WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
 
+get_random_image() {
+    dirlength=$(ls | wc -l)
+    if [ $dirlength -gt 0 ]; then
+        random_image=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+        if echo $(file $random_image) | grep -q -i "image"; then
+            echo $random_image
+        else
+            get_random_image
+        fi
+    elif [ $dirlength -eq 0 ]; then 
+        exit
+    fi
+}
+
 FILE=$1
 cd $WALLPAPER_DIR
 if [ -f "$FILE" ]; then
-    WALLPAPER=$FILE
-    echo "Setting wallpaper to $FILE"
-    feh --bg-fill $WALLPAPER
-    sleep 1
-    wal -i $WALLPAPER -n
-    xrdb merge ~/.cache/wal/dwm.Xresources
+    filedata=$(file $FILE)
+    if echo $filedata | grep -q -i "image"; then
+        WALLPAPER=$FILE
+        echo "Setting wallpaper to $FILE"
+        feh --bg-fill $WALLPAPER
+        sleep 1
+        wal -i $WALLPAPER -n
+        xrdb merge ~/.cache/wal/dwm.Xresources
+    else
+        echo "Given file not an image"
+        exit
+    fi
 else
-    WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-    echo "Setting random wallpaper"
-    feh --bg-fill $WALLPAPER
-    sleep 1
-    wal -i $WALLPAPER -n
-    xrdb merge ~/.cache/wal/dwm.Xresources
+    WALLPAPER=$(get_random_image)
+
+    if [ -z $WALLPAPER ]; then
+        echo "No image found in given folder"
+        exit
+    else
+        echo "Setting random wallpaper"
+        feh --bg-fill $WALLPAPER
+        sleep 1
+        wal -i $WALLPAPER -n
+        xrdb merge ~/.cache/wal/dwm.Xresources
+    fi
 fi
 
-[ -f "$HOME/.cache/wal/colors.sh" ] && . "$HOME/.cache/wal/colors.sh"
 killall dunst
 killall dwmblocks
-dwmblocks & /dev/null 2>&1
-/usr/bin/dunst & > /dev/null 2>&1 
+dwmblocks & 
+/usr/bin/dunst & 
     
-
 echo "The Current Wallpaper is: ${WALLPAPER}"
 
 
